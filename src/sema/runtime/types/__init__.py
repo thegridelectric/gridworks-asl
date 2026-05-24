656c3c08c1f4d6bc18919f02141d7a40b725893f
Jessica Millar <jessica.lynn.millar@gmail.com>
2026-05-24 13:05:23 -0400

    add weather type v000

--- src/sema/runtime/types/__init__.py
+++ src/sema/runtime/types/__init__.py
@@ -62,6 +62,7 @@ from sema.runtime.types.ticklist_hall import TicklistHall
 from sema.runtime.types.ticklist_hall_report import TicklistHallReport
 from sema.runtime.types.ticklist_reed import TicklistReed
 from sema.runtime.types.ticklist_reed_report import TicklistReedReport
+from sema.runtime.types.weather import Weather
 from sema.runtime.types.weather_forecast import WeatherForecast
 
 __all__ = [
@@ -129,5 +130,6 @@ __all__ = [
     "TicklistHallReport",
     "TicklistReed",
     "TicklistReedReport",
+    "Weather",
     "WeatherForecast",
 ]
