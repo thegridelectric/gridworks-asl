371514dd6e77685f6038337a3b856979b3c9ecf6
Jessica Millar <jessica.lynn.millar@gmail.com>
2026-08-25 19:20:50 -0400

    some more patches

--- src/sema/runtime/types/old_versions/relay_actor_config_002.py
+++ src/sema/runtime/types/old_versions/relay_actor_config_002.py
@@ -28,7 +28,7 @@ class RelayActorConfig002(SemaType):
     event_type: LeftRightDot
     de_energizing_event: NonEmptyString
     energizing_event: NonEmptyString
-    state_type: LeftRightDot
+    state_type: LeftRightDot | None = None
     de_energized_state: NonEmptyString
     energized_state: NonEmptyString
     type_name: Literal["relay.actor.config"] = "relay.actor.config"
