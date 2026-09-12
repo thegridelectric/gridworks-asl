bdaf9b3f1da13fc29e981c27c644b2d8f8dc5a8a
Jessica Millar <jessica.lynn.millar@gmail.com>
2026-09-12 08:34:01 -0400

    promote to staging; add baseurl.failure.alert

--- src/sema/runtime/types/__init__.py
+++ src/sema/runtime/types/__init__.py
@@ -4,6 +4,7 @@ from sema.runtime.types.ads_channel_config import AdsChannelConfig
 from sema.runtime.types.analog_dispatch import AnalogDispatch
 from sema.runtime.types.async_btu_params import AsyncBtuParams
 from sema.runtime.types.atn_bid import AtnBid
+from sema.runtime.types.baseurl_failure_alert import BaseurlFailureAlert
 from sema.runtime.types.bid import Bid
 from sema.runtime.types.capture_tuning import CaptureTuning
 from sema.runtime.types.channel_config import ChannelConfig
@@ -213,6 +214,7 @@ __all__ = [
     "AnalogDispatch",
     "AsyncBtuParams",
     "AtnBid",
+    "BaseurlFailureAlert",
     "Bid",
     "CaptureTuning",
     "ChannelConfig",
